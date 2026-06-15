087e0a62b923255e3ce37edcce5b6994cc3e4c01
Vinod-Ankareddy <51371557+Vinod-Ankareddy@users.noreply.github.com>
2026-06-15 20:51:42 +0530

    Delete udai-startup directory

--- udai-startup/src/pages/Dashboard/Dashboard.jsx
+++
@@ -1,7 +0,0 @@
-import DashboardComponent from "../../components/Dashboard";
-
-const Dashboard = () => {
-    return <DashboardComponent />
-}
-
-export default Dashboard;
--- udai-startup/src/pages/OTPLogin/OTPLogin.jsx
+++
@@ -1,9 +0,0 @@
-import OtpLoginComponent from "../../components/OtpLogin";
-
-const OtpLogin = () => {
-    return (
-        <OtpLoginComponent />
-    )
-}
-
-export default OtpLogin;
